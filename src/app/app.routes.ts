b7fef4d779f99be6cf0efd1a5d22df40461f9946
nereame96 <nereame96@gmail.com>
2026-01-07 18:10:00 +0100

    feat: create home component and routing to movies

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,13 @@
 import { Routes } from '@angular/router';
 import { MovieListComponent } from './components/movie-list/movie-list';
 import { MovieInfoComponent } from './components/movie-info/movie-info';
+import { HomeComponent } from './components/home/home';
 
 export const routes: Routes = [
    {
     path: '',
-    redirectTo: 'movies',
-    pathMatch: 'full'
+    component: HomeComponent
+
   },
 
   {
